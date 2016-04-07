33996ac6fb2347e54b0eaa06c1227759fe779f85
Hung Tran <hung_duy_tran@yahoo.com>
2016-04-06 22:35:57 -0700

    screen shot and input output dividers

--- app.js
+++ app.js
@@ -9,43 +9,53 @@ program
  .option('-p, --pattern <pattern>', 'Input Pattern such as . ,')
  .parse(process.argv);
 
-// input 1: the pattern, validate if exists
-if (!program.pattern) {
-	program.help();
-	process.exit();
-}
-
 // Create an input stream from the file system.
 var inputStream = fileSystem.createReadStream( "input-sensor.txt" );
 
-// input 2: the sensor file - validate if exists
-inputStream.on("error", function(err) {
-	if (err.code == 'ENOENT') {
-		console.log("input-sensor.txt required but not found.");
-	} else {
-		console.log(err);
+// closures simply for readability
+/************** validation ************/
+(function() {
+	// input 1: the pattern, validate if exists
+	if (!program.pattern) {
+		program.help();
+		process.exit();
 	}
-	
-	process.exit();
-});
+
+	// input 2: the sensor file - validate if exists
+	inputStream.on("error", function(err) {
+		if (err.code == 'ENOENT') {
+			console.log("input-sensor.txt required but not found.");
+		} else {
+			console.log(err);
+		}
+		
+		process.exit();
+	});
+})();
 
 /******************** start reading stream ******************/
-// Create a Pattern Matching stream that will run through the input and find matches
-// for the given pattern - "." and “,”.
-var patternStream = inputStream.pipe( new PatternMatch(program.pattern));
-// Read matches from the stream.
 
-var data = [];
-patternStream.on("readable", function() {
-	var content = null;
+(function() {
+	console.log("\n\n########################### Input ##########################");
+	inputStream.pipe(process.stdout);
 
-	while ( content = this.read() ) {
-		data.push(content);
-	}
+	// Create a Pattern Matching stream that will run through the input and find matches
+	// for the given pattern - "." and “,”.
+	var patternStream = inputStream.pipe( new PatternMatch(program.pattern));
+	// Read matches from the stream.
 
-});
+	var data = [];
+	patternStream.on("readable", function() {
+		var content = null;
 
-patternStream.on("end", function() {
-	console.log(data);
-	data = []; // reset 
-});
+		while ( content = this.read() ) {
+			data.push(content);
+		}
+
+	});
+
+	patternStream.on("end", function() {
+		console.log("\n\n########################### Output ##########################");
+		console.log(data); 
+	});
+})();
